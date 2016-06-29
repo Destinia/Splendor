bef44dbb586a2528f0050d1f9ef0f5ea06fc023d
Allen Lin <allen830423@gmail.com>
2016-06-29 22:53:44 +0800

    add /public/images/

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const webpack = require('webpack');
 const config = require('./webpack.config');
 const port = process.env.PORT || 3000;
+const domain = (process.argv[2] === '-domain') ? process.argv[3] : 'localhost';
 require('./server/app');
 
 const app = express();
@@ -24,11 +25,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, 'localhost', (err) => {
+app.listen(port, domain, (err) => {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://${domain}:${port}`);
 });
